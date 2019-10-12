4bf87a094a10543f3a3ea0d394d7555fa5b00f2b
Your Name <you@example.com>
2019-10-12 17:52:47 +0400

    first_commit

--- newPost.js
+++ newPost.js
@@ -1,10 +1,10 @@
 const onload= function () {
-    console.log("LOADED!")
+    console.log("LOADED!");
+    GetArticle();
 }
 
 var modal = document.getElementById("myModal");
 var inp = document.getElementById("str");
-var counter = 0;
 function Create(){
     console.log("Ready to create");
     modal.style.display = "block";
@@ -22,36 +22,69 @@ window.onclick = function(event) {
       modal.style.display = "none";
     }
   }
+const Delete= function(span){
+     let ind = span.childNodes;
+     console.log(ind[1].innerHTML);
+     span.parentNode.parentNode.removeChild(span.parentNode);
+     DeleteArticle(ind[1].innerHTML);
+}
 
 const Add = function (){
     const mainDiv = document.getElementById("container3_2");
     const post = document.createElement('div');
     const str = document.getElementById("str");
-    const span = document.createElement('span');
-    span.setAttribute('onclick', "Delete(this)")
-    span.className = "close";
-    span.innerHTML ="&minus;";
-    span.style.float = "right";
+    
     post.style.display = "flex";
-    post.style.justifyContent = "space-between"
     post.className = 'item';
 if(str.value!== ""){
-    counter ++;
-    inp.style.border = "1px solid #888";
+    console.log(str.value);
+    PostArticle({"str": `${str.value}`});
+
     post.innerHTML = str.value;
-    inp.value = "";
-    post.appendChild(span);
     mainDiv.appendChild(post);
-    PostArticle({str});
+    
+    inp.style.border = "1px solid #888";
+    inp.value = "";
+    inp.setAttribute('placeholder', "Reload page for delete post!");
     }
 else {
     inp.style.border = "1px solid red";
     }
   }
-  const Delete= function(post){
-      post.parentNode.parentNode.removeChild(post.parentNode);
-      DeleteArticle(counter);
-      counter--;
+  function GetArticle(){
+    return fetch ("https://5d9b4390686ed000144d1ed9.mockapi.io/js/base1")
+    .then(response => response.json())
+    .then(data => {
+    const mainDiv = document.getElementById("container3_2");
+    
+    for(let i=0;i< data.length;i++){
+      const post = document.createElement('div');
+      const span = document.createElement('span');
+      const ind = document.createElement('p'); // For save ID in HTML  
+      
+      span.setAttribute('onclick', "Delete(this)")
+      span.className = "close";
+      span.innerHTML ="&minus;";
+      span.style.float = "right";
+      
+      post.style.display = "flex";
+      post.style.justifyContent = "space-between"
+      post.className = 'item';
+      post.innerHTML = data[i].str;
+     
+      ind.style.display = "none";
+      ind.innerHTML = data[i].id;
+     
+      span.appendChild(ind);
+      post.appendChild(span);
+      mainDiv.appendChild(post);
+      console.log(data[i], i )
+    
+    }
+    
+    })
+
+    
   }
    
   const PostArticle = function (value) {   // Post 
@@ -59,7 +92,10 @@ else {
       "https://5d9b4390686ed000144d1ed9.mockapi.io/js/base1", 
       {
         method:"POST",
-        body: JSON.stringify(value)
+        body: JSON.stringify(value),
+        headers: {
+          'content-type': 'application/json'
+      }
       })
       .then(response => console.log("CREATED!",response, value))
   
